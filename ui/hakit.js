e110db02dacd4a83f51d6d25d9e5ac9a52c8142d
Sylvain Giroudon <giroudon@hakit.net>
2015-09-23 14:01:13 +0200

    ENH: Housekeeping in JS libraries

--- ui/hakit.js
+++ ui/hakit.js
@@ -1,13 +1,20 @@
+/*
+ * HAKit - The Home Automation Kit
+ * Copyright (C) 2014 Sylvain Giroudon
+ *
+ * JS WebSocket client library
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
 
-const ST_IDLE = 0;
-const ST_READY = 1;
-const ST_GET_CMD = 2;
-const ST_GET_RSP = 3;
+const HAKIT_ST_IDLE = 0;
+const HAKIT_ST_READY = 1;
+const HAKIT_ST_GET = 2;
 
 var hakit_sock;
-var hakit_sock_state = ST_IDLE;
-var hakit_signals;
-var hakit_dashboard;
+var hakit_sock_state = HAKIT_ST_IDLE;
 
 
 function get_appropriate_ws_url()
@@ -44,255 +51,20 @@ function hakit_send(msg) {
     }
 }
 
-function hakit_update(id, value) {
-    hakit_send('set '+id+'="'+value+'"');
+function hakit_set(name, value) {
+    hakit_send('set '+name+'="'+value+'"');
 }
 
 
-function clear_signals()
-{
-    if (hakit_signals) {
-	while (hakit_signals.rows.length > 1) {
-	    hakit_signals.deleteRow(1);
+function hakit_get(name) {
+    if (hakit_sock_state == HAKIT_ST_READY) {
+	if (name) {
+	    hakit_send("get "+name);
 	}
-    }
-}
-
-
-function signal_update(elmt, st)
-{
-    var id = elmt.id.split(':')[0];
-    console.log("signal_update "+id+" "+st);
-    hakit_update(id, st);
-}
-
-
-function switch_clicked(elmt)
-{
-    var st = elmt.checked ? 1:0;
-    signal_update(elmt, st);
-}
-
-
-function switch_release(elmt)
-{
-    elmt.checked = false;
-    signal_update(elmt, 0);
-}
-
-
-function widget_led(widget, value)
-{
-    if ((value != '') && (value != '0')) {
-	str = '<div class="led '+widget+'"></div>';
-    }
-    else {
-	str = '<div class="led led"></div>';
-    }
-
-    return str;
-}
-
-
-function widget_switch_slide(id, value)
-{
-    var str = '<div class="switch slide"><input type="checkbox" id="'+id+':switch" onclick="switch_clicked(this);"';
-    if ((value != '0') && (value != '')) {
-	str += ' checked';
-    }
-    str += '><label><i></i></label></div>';
-
-    return str;
-}
-
-
-function widget_switch_push(id, value)
-{
-    var str = '<div class="switch push"><input type="checkbox" id="'+id+':switch" onmousedown="signal_update(this,1);" onmouseup="switch_release(this);" onmouseout="switch_release(this);";';
-    if ((value != '0') && (value != '')) {
-	str += ' checked';
-    }
-    str += '><label><i></i></label></div>';
-
-    return str;
-}
-
-
-function widget_slider(id, value, options)
-{
-    var str = '<input type="range" class="slider" id="'+id+':slider" onchange="signal_update(this,this.value);"';
-
-    if (value) {
-	str += ' value="'+value+'"';
-    }
-
-    var list = options.split(',');
-    for (var i = 0; i < list.length; i++) {
-	str += ' '+list[i];
-    }
-
-    str += ' />';
-
-    return str;
-}
-
-
-function widget_meter(id, value, options)
-{
-
-    var str = '<meter id="'+id+':meter"';
-
-    if (value) {
-	str += ' value="'+value+'"';
-    }
-
-    var list = options.split(',');
-    for (var i = 0; i < list.length; i++) {
-	str += ' '+list[i];
-    }
-
-    str += '>'+value+'</meter>';
-
-    return str;
-}
-
-
-function add_signal(line)
-{
-    console.log("add_signal('"+line+"')");
-
-    var fields = line.split(" ");
-    var dir = fields[0];
-    var widget = fields[1];
-    var name = fields[2];
-    var value = fields[3];
-
-    if (hakit_dashboard) {
-	dashboard_update(name, value);
-    }
-
-    if (hakit_signals) {
-	var len = hakit_signals.rows.length;
-	var row = hakit_signals.insertRow(len);
-	if (len % 2) {
-	    row.className = "pure-table-odd";
+	else {
+	    hakit_send("get");
 	}
-
-	row.insertCell(0).innerHTML = dir;
-	row.insertCell(1).innerHTML = name;
-	row.insertCell(2).innerHTML = value;
-
-	var str = "";
-	if (widget.substr(0,4) == 'led-') {
-	    str = widget_led(widget, value);
-	}
-	else if (widget == 'switch-slide') {
-	    str = widget_switch_slide(name, value);
-	}
-	else if (widget == 'switch-push') {
-	    str = widget_switch_push(name, value);
-	}
-	else if (widget.substr(0,5) == 'meter') {
-	    var args = widget.split(':');
-	    str = widget_meter(name, value, args[1]);
-	}
-	else if (widget.substr(0,6) == 'slider') {
-	    var args = widget.split(':');
-	    str = widget_slider(name, value, args[1]);
-	}
-
-	row.insertCell(3).innerHTML = str;
-
-	var cell = row.insertCell(4);
-	cell.hidden = true;
-	cell.innerHTML = widget;
-    }
-}
-
-
-function update_signal(line)
-{
-    console.log("update_signal('"+line+"')");
-
-    var i = line.indexOf(" ");
-    if (i <= 0) {
-	return;
-    }
-
-    var name = line.substr(0,i);
-    var value = line.substr(i+1);
-
-    if (hakit_dashboard) {
-	dashboard_update(name, value);
-    }
-
-    if (hakit_signals) {
-	var rows = hakit_signals.rows;
-	// Update row with new value
-	for (var i = 0; i < rows.length; i++) {
-	    var row = rows[i];
-
-	    if (row.cells[1].innerHTML == name) {
-		row.cells[2].innerHTML = value;
-		var widget = row.cells[4].innerHTML;
-
-		if (widget.substr(0, 4) == 'led-') {
-		    row.cells[3].innerHTML = widget_led(widget, value);
-		}
-		else {
-		    var elmt = document.getElementById(name+':switch');
-		    if (elmt) {
-			elmt.checked = ((value != '') && (value != '0'));
-		    }
-
-		    elmt = document.getElementById(name+':meter');
-		    if (elmt) {
-			elmt.value = value;
-		    }
-
-		    elmt = document.getElementById(name+':slider');
-		    if (elmt) {
-			elmt.value = value;
-		    }
-		}
-		return;
-	    }
-	}
-
-	// If row not found, refresh the whole list
-	get_all();
-    }
-}
-
-
-function recv_line(line) {
-    //console.log("recv_line('"+line+"')");
-
-    if (line == ".") {
-	hakit_sock_state = ST_READY;
-    }
-    else if (line.substr(0,1) == "!") {
-	update_signal(line.substr(1));
-    }
-    else {
-	switch (hakit_sock_state) {
-	    case ST_READY:
-	        break;
-	    case ST_GET_CMD:
-	        hakit_sock_state = ST_GET_RSP;
-	        clear_signals();
-	    case ST_GET_RSP:
-	        add_signal(line);
-	        break;
-	}
-    }
-}
-
-
-function get_all() {
-    if (hakit_sock_state == ST_READY) {
-	hakit_send("get");
-	hakit_sock_state = ST_GET_CMD;
+	hakit_sock_state = HAKIT_ST_GET;
     }
     else {
 	console.log("WARNING: Attempting to send command in sock state "+hakit_sock_state);
@@ -300,11 +72,40 @@ function get_all() {
 }
 
 
-function hakit_connect() {
-    /* Get document elements */
-    hakit_signals = document.getElementById("signals");
-    hakit_dashboard = document.getElementById("dashboard");
+function hakit_recv_line(line) {
+    //console.log("hakit_recv_line('"+line+"')");
 
+    if (line == ".") {
+	hakit_sock_state = HAKIT_ST_READY;
+    }
+    else if (line.substr(0,1) == "!") {
+	var i = line.indexOf(" ");
+	if (i > 1) {
+	    var name = line.substr(1,i-1);
+	    var value = line.substr(i+1);
+	    hakit_updated(name, value);
+	}
+    }
+    else {
+	if (hakit_sock_state == HAKIT_ST_GET) {
+	    var fields = line.split(" ");
+	    var dir = fields[0];
+	    var widget = fields[1];
+	    var name = fields[2];
+
+	    var value = '';
+	    for (var i = 3; i < fields.length; i++) {
+		value += ' '+fields[i];
+	    }
+	    value = value.trim();
+
+	    hakit_updated(name, value, dir, widget);
+	}
+    }
+}
+
+
+function hakit_connect() {
     /* Setup WebSocket connection */
     if (typeof MozWebSocket != "undefined") {
 	hakit_sock = new MozWebSocket(get_appropriate_ws_url(), "hakit-events-protocol");
@@ -314,16 +115,10 @@ function hakit_connect() {
 
     try {
 	hakit_sock.onopen = function() {
-	    hakit_sock_state = ST_READY;
-
-	    if (hakit_signals) {
-		document.getElementById("ws_status_td").style.backgroundColor = "#40ff40";
-		document.getElementById("ws_status").textContent = "Connected";
-	    }
-	    if (hakit_dashboard) {
-		dashboard_connect(true);
-	    }
-	    get_all();
+	    hakit_sock_state = HAKIT_ST_READY;
+	    console.log("hakit_connect: connection established");
+	    hakit_connected(true);
+	    hakit_get();
 	} 
 
 	hakit_sock.onmessage = function got_packet(msg) {
@@ -332,22 +127,16 @@ function hakit_connect() {
 	    for (var i = 0; i < lines.length; i++) {
 		var line = lines[i];
 		if (line != "") {
-		    recv_line(line);
+		    hakit_recv_line(line);
 		}
 	    }
 	    //console.log("===");
 	} 
 
 	hakit_sock.onclose = function(){
-	    hakit_sock_state = ST_IDLE;
-
-	    if (hakit_signals) {
-		document.getElementById("ws_status_td").style.backgroundColor = "#ff4040";
-		document.getElementById("ws_status").textContent = "Disconnected";
-	    }
-	    if (hakit_dashboard) {
-		dashboard_connect(false);
-	    }
+	    hakit_sock_state = HAKIT_ST_IDLE;
+	    console.log("hakit_connect: connection close");
+	    hakit_connected(false);
 	}
     } catch(exception) {
 	alert('<p>ERROR: ' + exception + '</p>');  
---
+++ ui/hakit_signal.js
@@ -0,0 +1,236 @@
+/*
+ * HAKit - The Home Automation Kit
+ * Copyright (C) 2014 Sylvain Giroudon
+ *
+ * Dynamic table of source/sink signals
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+var hakit_signals;
+
+function hakit_widget_updated(elmt, st)
+{
+    var id = elmt.id.split(':')[0];
+    console.log("hakit_widget_updated "+id+" "+st);
+    hakit_set(id, st);
+}
+
+
+function hakit_widget_clicked(elmt)
+{
+    var st = elmt.checked ? 1:0;
+    hakit_widget_updated(elmt, st);
+}
+
+
+function hakit_widget_release(elmt)
+{
+    elmt.checked = false;
+    hakit_widget_updated(elmt, 0);
+}
+
+
+function hakit_widget_led(widget, value)
+{
+    if ((value != '') && (value != '0')) {
+	str = '<div class="led '+widget+'"></div>';
+    }
+    else {
+	str = '<div class="led led"></div>';
+    }
+
+    return str;
+}
+
+
+function hakit_widget_switch_slide(id, value)
+{
+    var str = '<div class="switch slide"><input type="checkbox" id="'+id+':switch" onclick="hakit_widget_clicked(this);"';
+    if ((value != '0') && (value != '')) {
+	str += ' checked';
+    }
+    str += '><label><i></i></label></div>';
+
+    return str;
+}
+
+
+function hakit_widget_switch_push(id, value)
+{
+    var str = '<div class="switch push"><input type="checkbox" id="'+id+':switch" onmousedown="hakit_widget_updated(this,1);" onmouseup="hakit_widget_release(this);" onmouseout="hakit_widget_release(this);"';
+    if ((value != '0') && (value != '')) {
+	str += ' checked';
+    }
+    str += '><label><i></i></label></div>';
+
+    return str;
+}
+
+
+function hakit_widget_slider(id, value, options)
+{
+    var str = '<input type="range" class="slider" id="'+id+':slider" onchange="hakit_widget_updated(this,this.value);"';
+
+    if (value) {
+	str += ' value="'+value+'"';
+    }
+
+    var list = options.split(',');
+    for (var i = 0; i < list.length; i++) {
+	str += ' '+list[i];
+    }
+
+    str += ' />';
+
+    return str;
+}
+
+
+function hakit_widget_meter(id, value, options)
+{
+
+    var str = '<meter id="'+id+':meter"';
+
+    if (value) {
+	str += ' value="'+value+'"';
+    }
+
+    var list = options.split(',');
+    for (var i = 0; i < list.length; i++) {
+	str += ' '+list[i];
+    }
+
+    str += '>'+value+'</meter>';
+
+    return str;
+}
+
+
+function hakit_signal_clear()
+{
+    if (hakit_signals) {
+	while (hakit_signals.rows.length > 1) {
+	    hakit_signals.deleteRow(1);
+	}
+    }
+}
+
+
+function hakit_signal_init()
+{
+    /* Get signal table element */
+    hakit_signals = document.getElementById("signals");
+    hakit_connect();
+}
+
+
+function hakit_signal_row(name)
+{
+    var rows = hakit_signals.rows;
+    for (var i = 0; i < rows.length; i++) {
+	var row = rows[i];
+	if (row.cells[1].innerHTML == name) {
+	    return row;
+	}
+    }
+
+    return null;
+}
+
+
+function hakit_signal_add(name, value, dir, widget)
+{
+    console.log('hakit_signal_add(name="'+name+'", value="'+value+'", dir='+dir+', widget='+widget+')');
+
+    var len = hakit_signals.rows.length;
+    var row = hakit_signals.insertRow(len);
+    if (len % 2) {
+	row.className = "pure-table-odd";
+    }
+
+    row.insertCell(0).innerHTML = dir;
+    row.insertCell(1).innerHTML = name;
+    row.insertCell(2).innerHTML = value;
+
+    var str = "";
+    if (widget.substr(0,4) == 'led-') {
+	str = hakit_widget_led(widget, value);
+    }
+    else if (widget == 'switch-slide') {
+	str = hakit_widget_switch_slide(name, value);
+    }
+    else if (widget == 'switch-push') {
+	str = hakit_widget_switch_push(name, value);
+    }
+    else if (widget.substr(0,5) == 'meter') {
+	var args = widget.split(':');
+	str = hakit_widget_meter(name, value, args[1]);
+    }
+    else if (widget.substr(0,6) == 'slider') {
+	var args = widget.split(':');
+	str = hakit_widget_slider(name, value, args[1]);
+    }
+
+    row.insertCell(3).innerHTML = str;
+
+    var cell = row.insertCell(4);
+    cell.hidden = true;
+    cell.innerHTML = widget;
+}
+
+
+function hakit_updated(name, value, dir, widget)
+{
+    console.log('hakit_updated(name="'+name+'", value="'+value+'")');
+
+    var row = hakit_signal_row(name);
+    if (row) {
+	// If row exists, update its value
+	row.cells[2].innerHTML = value;
+	var widget = row.cells[4].innerHTML;
+
+	if (widget.substr(0, 4) == 'led-') {
+	    row.cells[3].innerHTML = hakit_widget_led(widget, value);
+	}
+	else {
+	    var elmt = document.getElementById(name+':switch');
+	    if (elmt) {
+		elmt.checked = ((value != '') && (value != '0'));
+	    }
+
+	    elmt = document.getElementById(name+':meter');
+	    if (elmt) {
+		elmt.value = value;
+	    }
+
+	    elmt = document.getElementById(name+':slider');
+	    if (elmt) {
+		elmt.value = value;
+	    }
+	}
+	return;
+    }
+    else {
+	/* Append a new row */
+	hakit_signal_add(name, value, dir, widget);
+    }
+}
+
+
+function hakit_connected(connected)
+{
+    console.log("hakit_connected("+connected+")");
+
+    if (connected) {
+	document.getElementById("ws_status_td").style.backgroundColor = "#40ff40";
+	document.getElementById("ws_status").textContent = "Connected";
+	hakit_signal_clear();
+    }
+    else {
+	document.getElementById("ws_status_td").style.backgroundColor = "#ff4040";
+	document.getElementById("ws_status").textContent = "Disconnected";
+    }
+}
